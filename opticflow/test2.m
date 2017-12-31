53884cc61c6c7b45a87046e3c9d68fd73a8d192e
zhearing <zzy19950621@gmail.com>
2017-12-31 11:37:43 +0800

    add figure

--- opticflow/test2.m
+++ opticflow/test2.m
@@ -20,9 +20,9 @@ for i=1:N_image
 end
 
 subplot(331);
-imshow(Gray_image{1,8}),title('(1)参考图像');
+imshow(Gray_image{1,8}),title('fig.1 reference image');
 subplot(332);
-imshow(Gray_image{1,4}),title('(2)原始图像');
+imshow(Gray_image{1,4}),title('fig.2 original image');
 
 %% 计算光流信息
 Iinputg = Gray_image{1,8};
@@ -34,7 +34,7 @@ converter = vision.ImageDataTypeConverter;
 % 修改光流对象的配置
 opticalFlow.OutputValue = 'Horizontal and vertical components in complex form'; % 返回复数形式光流图
 opticalFlow.ReferenceFrameSource = 'Input port'; % 对比两张图片，而不是视频流
-if 1 % 使用的算法
+if 0 % 使用的算法
     opticalFlow.Method = 'Lucas-Kanade';
     opticalFlow.NoiseReductionThreshold = 0.01; % 默认是0.0039
 else
@@ -53,12 +53,12 @@ flow_H = real(opticflow);
 flow_V = imag(opticflow);
 flow_cc = computeColor(flow_H, flow_V);
 subplot(333)
-imshow(flow_cc),title('(3)光流场的彩色显示');
+imshow(flow_cc),title('fig.3 the color flow of optical field');
 
 % 光流场的灰度显示
 flow_gray = 255 - rgb2gray(flow_cc);
 subplot(334);
-imshow(flow_gray),title('(4)光流场的灰度显示');
+imshow(flow_gray),title('fig.4 the optical flow of the gray-scale');
 
 threshold = 45;
 New_image = flow_gray;
@@ -74,7 +74,7 @@ end
 flow_gray = New_image;
 
 subplot(335);
-imshow(flow_gray),title('(5)二值化后的光流场的灰度显示');
+imshow(flow_gray),title('fig.5 gray-scale of the binarized optical flow field');
 
 %% 腐蚀和膨胀
 se1 = strel('square',8);
@@ -82,9 +82,9 @@ se0 = strel('square',1);
 flow_gray = imdilate(flow_gray, se1);
 flow_gray = imerode(flow_gray,se0);
 subplot(336);
-imshow(flow_gray),title('(6)形态学处理后的光流场的灰度显示');
+imshow(flow_gray),title('fig.6 Morphology of the optical flowafter the gray-scale');
 
 %% 标记
 Image = mark(Iinputg, flow_gray);
 subplot(337)
-imshow(Image),title('(7)运动目标分割');
+imshow(Image),title('fig.7 moving target');
